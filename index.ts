a4cf54f5625e538a23e05f42239dbde8b59e8c02
Bowei Liu <liubowei@gmail.com>
2024-03-22 10:37:26 -0700

    exp

--- index.ts
+++ index.ts
@@ -2,6 +2,8 @@ import main from './pty';
 // https://github.com/EDDYMENS/interactive-terminal/blob/main/backend.js
 
 const WebSocket = require('ws');
+const express = require('express');
+const path = require('path');
 
 
 //main();
@@ -9,4 +11,14 @@ const WebSocket = require('ws');
 const wss = new WebSocket.Server({ port: 6060  });
 console.log('websocket up on 6060');
 
+
+
+const app = express();
+const PORT = process.env.PORT || 6000;
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.listen(PORT, () => {
+    console.log('express up on 6000');
+})
 
