68e01c31179f466c00228591dd5d5b50fa4fb331
Bowei Liu <liubowei@gmail.com>
2024-04-16 08:04:37 -0700

    partial port

--- pty-example-vite/src/server/main.ts
+++ pty-example-vite/src/server/main.ts
@@ -1,12 +1,24 @@
 import express from "express";
 import ViteExpress from "vite-express";
+import WebSocket from "ws";
+
+const WS_PORT = Number(process.env.WS_PORT || 6060);
+const wss = new WebSocket.Server({ port: WS_PORT });
+console.log('websocket up on ', WS_PORT);
+
+wss.on('connection', (ws: WebSocket) => {
+    ws.on('message', (command: string) => {
+        console.log('got ws message', JSON.stringify(command));
+    });
+})
+
+
+// app.get("/hello", (_, res) => {
+//   res.send("Hello Vite + TypeScript!");
+// });
 
 const app = express();
-
-app.get("/hello", (_, res) => {
-  res.send("Hello Vite + TypeScript!");
-});
-
-ViteExpress.listen(app, 3000, () =>
-  console.log("Server is listening on port 3000..."),
+const PORT = Number(process.env.PORT || 6080);
+ViteExpress.listen(app, PORT, () =>
+  console.log("Server is listening on port ", PORT)
 );
