d7d3ae890bb7619d53a1290f5bcfb62c9aa1f7a0
Bowei Liu <liubowei@gmail.com>
2024-04-28 17:35:59 -0700

    hook up some xterm to vite

--- pty-example-vite/src/client/main.ts
+++ pty-example-vite/src/client/main.ts
@@ -2,6 +2,7 @@ import "./style.css";
 
 import { setupCounter } from "./counter";
 import typescriptLogo from "./typescript.svg";
+import { Terminal  } from '@xterm/xterm';
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <div>
@@ -22,3 +23,8 @@ document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
 `;
 
 setupCounter(document.querySelector<HTMLButtonElement>("#counter")!);
+
+const term = new Terminal();
+term.open(document.getElementById('terminal')!);
+term.write('Hello from \x1B[1;3;31mxterm.js\x1B[0m $ ');
+
