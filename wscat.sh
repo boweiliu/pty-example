018e7fde1837c93bec16c902dbf4adb095ba8da4
Bowei Liu <liubowei@gmail.com>
2024-04-28 18:08:10 -0700

    ok maybe

--- wscat.sh
+++ wscat.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # uses wscat to test whether the websocket server is working.
+# NOTE: wscat seems to be unable to send the EOL character "\r", so this can't be used to test pty
 
 which wscat || sudo npm install -g wscat
 
 wscat -c ws://localhost:6060
+
